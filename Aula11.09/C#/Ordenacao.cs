18869d9d0859ae7d7a780b10f780bb951704b85e
Iago-Vargas <166236052+Iago-Vargas@users.noreply.github.com>
2024-09-11 10:39:23 -0300

    Add files via upload

---
+++ Aula11.09/C#/Ordenacao.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1109
+{
+    internal class Ordenacao
+    {
+        public static void pente(List<Aluno> lista)
+        {
+            int distancia = lista.Count();
+            Aluno tmp;
+            int i;
+            bool houveTroca;
+
+            do
+            {
+                distancia = (int)(distancia / 1.3);
+                if (distancia < 1) distancia = 1;
+                houveTroca = false;
+                for (i = 0; i + distancia < lista.Count(); i++)
+                {
+                    if (lista[i].CompareTo(lista[i + distancia]) > 0)
+                    {
+                        houveTroca = true;
+                        tmp = lista[i];
+                        lista[i] = lista[i + distancia];
+                        lista[i + distancia] = tmp;
+                    }
+                }
+            } while (distancia > 1 || houveTroca);
+        }
+
+        public static void bolha(List<Aluno> lista)
+        {
+            Aluno tmp;
+            int i;
+            bool houveTroca;
+
+            do
+            {
+                houveTroca = false;
+                for (i = 0; i < lista.Count() - 1; i++)
+                {
+                    if (lista[i].CompareTo(lista[i + 1]) > 0)
+                    {
+                        houveTroca = true;
+                        tmp = lista[i];
+                        lista[i] = lista[i + 1];
+                        lista[i + 1] = tmp;
+                    }
+                }
+            } while (houveTroca);
+        }
+    }
+}
---
+++ Aula11.09/C#/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Aula1109;
+
+List<Aluno> lista = new List<Aluno>();
+
+lista.Add(new Aluno("Matheus", 19));
+lista.Add(new Aluno("Romeo", 19));
+lista.Add(new Aluno("Viti", 19));
+lista.Add(new Aluno("Gilberto", 20));
+lista.Add(new Aluno("Bruno", 19));
+lista.Add(new Aluno("Bruno", 18));
+lista.Add(new Aluno("Anthony", 20));
+lista.Add(new Aluno("Arthur", 19));
+
+//lista.Sort();
+//lista = lista.OrderBy(a => a.Nome).ThenBy(a => a.Idade).ToList();
+//Ordenacao.pente(lista);
+Ordenacao.bolha(lista);
+foreach (var item in lista)
+{
+    Console.WriteLine(item);
+}
